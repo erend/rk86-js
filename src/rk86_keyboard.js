71cb23b593952bed37360bc03741dfbdddd56223
Gleb Andreev <gleb.erend@gmail.com>
2021-08-11 12:25:33 +0300

    Try keycode 9

--- src/rk86_keyboard.js
+++ src/rk86_keyboard.js
@@ -158,7 +158,7 @@ function Keyboard() {
         }
       }
       this.meta_keys_buffer = null;
-    } else if (code == 27) {
+    } else if (code == 9) {
       console.log('27!');
       history.back();
     } else {
