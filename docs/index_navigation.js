9fb33f458836e598c9bf1520db29ac4231d914dd
Gleb Andreev <gleb.erend@gmail.com>
2021-10-13 14:33:04 +0300

    Cange about page

--- docs/index_navigation.js
+++ docs/index_navigation.js
@@ -6,25 +6,33 @@ function list_keyboard_navigation() {
 
   document.onkeydown = event => {
     const keyCode = event.keyCode;
-
     if (keyCode == 38 && active > 0) {      
       active = active - 1;
-
     }
-    if (keyCode == 40 && active < buttons.length) {      
+    if (keyCode == 40 && buttons.length > 1 && active < buttons.length) {      
       active = active + 1;
+    }
+    if (keyCode == 27) {
+      console.log('esc', window.location);
+      window.location.assign('http://127.0.0.1:8000/');
     }
 
     buttons.item(active).focus();
   }
 }
 
-function main_keyboard_navigation() {
-  document.onkeydown = event => {
-    const keyCode = event.keyCode;
-
-    if (keyCode == 27) {
-      window.history.back();
-    }
+function fill_brackets() {
+  const bracketsBlock = document.getElementByClassName("brackets");
+  
+  let brackets = '';
+  for (let i = 0; i < 19; i++) {
+    brackets = `${brackets}()`;    
   }
+  console.log('brackets', bracketsBlock, brackets);
+  bracketsBlock.innerHTML = brackets;
+}
+
+function combine_navigation() {
+  list_keyboard_navigation();
+  fill_brackets();
 }
